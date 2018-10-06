2cfb7da6265f3b8723c6aba262fd848f01b4b781
ptm23 <ptm@autograf.pl>
2018-10-06 10:27:21 +0200

    multiply objects

--- json2xml/json2xml.py
+++ json2xml/json2xml.py
@@ -1,19 +1,28 @@
-import dicttoxml
 import json
 import xml.etree.cElementTree as ET
 import os
 from PIL import Image
 from shutil import copyfile
 
-ann_dir = "/home/masta/Pictures/spark_huge/out/"
-img_dir = "/home/masta/Pictures/spark_huge/"
+in_label = 'cat'
+out_label = 'siam'
 
-final_img_dir = "/home/masta/Pictures/spark_huge_final/images/"
-final_ann_dir = "/home/masta/Pictures/spark_huge_final/ann/"
+img_dir = "C:/Users/Piotr/Desktop/cat_test/"
+ann_dir = img_dir + "out/"
+
+
+final_img_dir = "C:/Users/Piotr/Desktop/cat_test_out/images/"
+final_ann_dir = "C:/Users/Piotr/Desktop/cat_test_out/ann/"
+
+if not os.path.exists(final_img_dir):
+    os.makedirs(final_img_dir)
+
+if not os.path.exists(final_ann_dir):
+    os.makedirs(final_ann_dir)
 
 for file in os.listdir(ann_dir):
     statinfo = os.stat(ann_dir + file)
-    if file.endswith(".json") and 50 < statinfo.st_size < 200:
+    if file.endswith(".json") and 50 < statinfo.st_size:
         print(file)
         img_file_name = file.replace("json", "jpg")
         img_file_full_path = img_dir + img_file_name
@@ -32,49 +41,51 @@ for file in os.listdir(ann_dir):
         data = json.loads(f.read())
         f.close()
 
-        if data and 'car' in data[0]['label']:
+        annotation = ET.Element("annotation")
 
-            print(data[0])
-            print(data[0]['topleft']['x'])
+        # annotation
+        ET.SubElement(annotation, "folder").text = out_label
+        ET.SubElement(annotation, "filename").text = img_file_name
+        ET.SubElement(annotation, "path").text = img_file_full_path
 
-            annotation = ET.Element("annotation")
+        # annotation/source
+        source = ET.SubElement(annotation, "source")
+        ET.SubElement(source, "database").text = "Unknown"
 
-            #annotation
-            ET.SubElement(annotation, "folder").text = "audia6c6"
-            ET.SubElement(annotation, "filename").text = img_file_name
-            ET.SubElement(annotation, "path").text = img_file_full_path
+        # annotation/size
+        source = ET.SubElement(annotation, "size")
+        ET.SubElement(source, "width").text = str(width)
+        ET.SubElement(source, "height").text = str(height)
+        ET.SubElement(source, "depth").text = "3"
 
-            #annotation/source
-            source = ET.SubElement(annotation, "source")
-            ET.SubElement(source, "database").text = "Unknown"
+        # annotation/segmented
+        ET.SubElement(annotation, "segmented").text = "0"
 
-            #annotation/size
-            source = ET.SubElement(annotation, "size")
-            ET.SubElement(source, "width").text = str(width)
-            ET.SubElement(source, "height").text = str(height)
-            ET.SubElement(source, "depth").text = "3"
 
-            #annotation/segmented
-            ET.SubElement(annotation, "segmented").text = "0"
+        for d in data:
+            if d and in_label in d['label']:
 
-            #annotation/object
-            object = ET.SubElement(annotation, "object")
+                print(d)
+                print(d['topleft']['x'])
 
-            ET.SubElement(object, "name").text = "audia6c6fl"
-            ET.SubElement(object, "pose").text = "Unspecified"
-            ET.SubElement(object, "truncated").text = str(0)
-            ET.SubElement(object, "difficult").text = str(0)
+                # annotation/object
+                object = ET.SubElement(annotation, "object")
 
-            bndbox = ET.SubElement(object, "bndbox")
-            ET.SubElement(bndbox, "xmin").text = str(data[0]['topleft']['x'])
-            ET.SubElement(bndbox, "ymin").text = str(data[0]['topleft']['y'])
-            ET.SubElement(bndbox, "xmax").text = str(data[0]['bottomright']['x'])
-            ET.SubElement(bndbox, "ymax").text = str(data[0]['bottomright']['y'])
+                ET.SubElement(object, "name").text = out_label
+                ET.SubElement(object, "pose").text = "Unspecified"
+                ET.SubElement(object, "truncated").text = str(0)
+                ET.SubElement(object, "difficult").text = str(0)
 
-            tree = ET.ElementTree(annotation)
+                bndbox = ET.SubElement(object, "bndbox")
+                ET.SubElement(bndbox, "xmin").text = str(d['topleft']['x'])
+                ET.SubElement(bndbox, "ymin").text = str(d['topleft']['y'])
+                ET.SubElement(bndbox, "xmax").text = str(d['bottomright']['x'])
+                ET.SubElement(bndbox, "ymax").text = str(d['bottomright']['y'])
 
-            print(tree)
-            tree.write(final_ann_dir + file.replace("json", "xml"))
-            copyfile(img_file_full_path, final_img_dir + img_file_name)
-            #xml = dicttoxml.dicttoxml(data)
-            #print(xml)
+        tree = ET.ElementTree(annotation)
+
+        print(tree)
+        tree.write(final_ann_dir + file.replace("json", "xml"))
+        copyfile(img_file_full_path, final_img_dir + img_file_name)
+        #xml = dicttoxml.dicttoxml(data)
+        #print(xml)
